fbf73e0b28589463cd1d143fb6e80ee842570b83
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2018-08-26 13:19:54 +0700

    Update dellallphoto.js

--- dellallphoto.js
+++ dellallphoto.js
@@ -124,10 +124,11 @@ console.log(chalk`
   [?] {bold.green MASS DELETE ALL PHOTO IG *SET SLEEP!}
 
   ——————————————————  [THANKS TO]  ————————————————————
-  [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  [✓] FIXING & TESTING BY SYNTAX (@teamsyntaxid)
+  [✓] CODE BY CYBER SCREAMER BINTANG (@bintang_nur_pradana)
+  [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
   [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
-  [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
+  [✓] SGB TEAM REBORN | Zerobyte.id | @bintang_nur_pradana 
+  [✓] FIX 2018 BY @bintang_nur_pradana
   —————————————————————————————————————————————————————}
       `);
 
